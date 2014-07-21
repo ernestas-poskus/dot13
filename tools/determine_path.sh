16ead973227ba7dbed5199d97614423b952d9107
Ernestas Poskus <ernestas.poskus@gmail.com>
2014-07-21 21:27:54 +0300

    Add tools: determine_path

---
+++ tools/determine_path.sh
@@ -0,0 +1,8 @@
+DOT_PATH="$HOME/dot13"
+DOT_PATH_INSTALL="$DOT_PATH/install"
+
+echo "export DOT_PATH='$DOT_PATH'" >> ~/.dot13_environment
+echo "export DOT_PATH_INSTALL='$DOT_PATH_INSTALL'" >> ~/.dot13_environment
+echo "export DOT_PATH_LINUX='$DOT_PATH/os/linux'" >> ~/.dot13_environment
+echo "export DOT_PATH_DARWIN='$DOT_PATH/os/darwin'" >> ~/.dot13_environment
+
