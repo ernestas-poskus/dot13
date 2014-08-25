6f3e18a5a3853b63e80eb1c277fd41ec913a690b
Ernestas Poskus <ernestas@vinted.com>
2014-08-25 17:33:56 +0300

    Add work config & gitconfig auto

--- dot_hook.zsh
+++ dot_hook.zsh
@@ -3,7 +3,7 @@
 if [ -f "$HOME/.dot13_environment" ]; then
   source "$HOME/.dot13_environment"
 else
-  echo '.dot13_environment - Environment / Kernel exports missing'
+  echo '.dot13_environment - exports missing'
 fi
 
 # Centalized sourcefiles
@@ -13,6 +13,11 @@ for f ($HOME/dot13/sourcefiles/.*); do
   fi
 done
 unset $f
+
+# Work config
+if [ "$DOT13_ENV" = 'work' ]; then
+  ln -sf "$HOME/dot13/work/.gitconfig_vinted" "$HOME/.gitconfig"
+fi
 
 # OS specific source
 if [ "$DOT_KERNEL" = 'darwin' ]; then
@@ -24,5 +29,5 @@ if [ "$DOT_KERNEL" = 'linux' ]; then
 
   # Remap keys
   xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape' # Caps Lock -> ESC
-  xmodmap -e 'clear Lock' -e 'keycode 0x6c = BackSpace'
+  xmodmap -e 'clear Lock' -e 'keycode 0x6c = BackSpace' # R Alt -> Backspace
 fi
--- tools/determine_environment.sh
+++ tools/determine_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$(whoami)" == 'ernestasposkus' ]; then
-  echo 'export DOT_ENV="work"' >> ~/.dot13_environment
+  echo 'export DOT13_ENV="work"' >> ~/.dot13_environment
 else
-  echo 'export DOT_ENV="home"' >> ~/.dot13_environment
+  echo 'export DOT13_ENV="home"' >> ~/.dot13_environment
 fi
