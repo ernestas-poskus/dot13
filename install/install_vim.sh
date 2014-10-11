ddb60a7c3be1c30d82146ea998b0a6faa8b36799
Ernestas Poskus <ernestas.poskus@gmail.com>
2014-10-11 20:09:26 +0300

    Update ubuntu install_vim script

--- install/install_vim.sh
+++ install/install_vim.sh
@@ -14,36 +14,34 @@ if ! type -p vim > /dev/null; then
       brew cleanup
       brew install macvim --custom-icons --override-system-vim --with-lua --with-luajit
     elif [ "$DOT_KERNEL" == 'linux' ]; then
-      # Install lua from binaries (these are out-of-date but at least they worked).
-      sudo apt-get install lua50 liblua50-dev liblualib50-dev
 
       # Remove old vims
       sudo apt-get remove vim vim-runtime gvim
       sudo apt-get remove vim-tiny vim-common vim-gui-common
 
-      # Download and build a new vim
-      sudo apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev \
-      libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
-      libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev mercurial \
-      vim-nox
-      cd ~
+      sudo apt-get install liblua5.1-dev vim-nox
+      sudo mkdir /usr/include/lua5.1/include/
+      sudo cp /usr/include/lua5.1/* /usr/include/lua5.1/include/
+      sudo ln -s /usr/lib/x86_64-linux-gnu/liblua5.1.so /usr/local/lib/liblua.so
       hg clone https://code.google.com/p/vim/
       cd vim
-      cd ~/vim
       ./configure --with-features=huge \
-                  --enable-rubyinterp \
-                  --enable-pythoninterp \
-                  --with-python-config-dir=/usr/lib/python2.7-config \
-                  --enable-perlinterp \
-                  --enable-gui=gtk2 --enable-cscope --prefix=/usr \
-                  --enable-luainterp \
-		  --with-lua-prefix=/usr/local
-      echo "At this point, check the output of ./configure to see that it found lua.h."
-      echo "If not, find out where it is (I'm afraid I can't remember where it was). "
-      echo "Symlink to it in /usr/local with e.g. sudo ln -s ../lua.h and rerun ./configure."
-      echo ''
-      echo 'sudo make VIMRUNTIMEDIR=/usr/share/vim/vim74'
-      echo 'sudo make install'
+          --enable-perlinterp \
+          --enable-rubyinterp \
+          --enable-pythoninterp=yes \
+          --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
+          --enable-multibyte \
+          --enable-fontset \
+          --enable-gui=gtk2 \
+          --disable-netbeans \
+          --enable-luainterp=yes \
+          --with-lua-prefix=/usr/include/lua5.1 \
+          --enable-cscope \
+          --enable-largefile \
+          --prefix=/usr
+      make VIMRUNTIMEDIR=/usr/share/vim/vim74
+      sudo checkinstall
+      make install
     fi
 
   fi
