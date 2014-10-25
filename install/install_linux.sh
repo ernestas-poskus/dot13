53b3a7af4fad3d85485ff4421c421be4425658d4
Ernestas Poskus <ernestas.poskus@gmail.com>
2014-10-25 17:40:13 +0300

    Add Linux dependencies

--- install/install_linux.sh
+++ install/install_linux.sh
@@ -3,6 +3,9 @@
 # Update sources
 sudo apt-get install update
 
+# Libraries
+sudo apt-get install autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev
+
 # Vim Ag
 sudo apt-get install silversearcher-ag
 
