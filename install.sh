85e8dbac120413f319c9e98310f659d1c3d3b3d8
Ernestas Poskus <ernestas.poskus@gmail.com>
2015-09-14 20:54:26 +0300

    install.sh fallback to upgrade if installed

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ ! -d "$HOME/dot13" ]; then
     echo "Installing dot13"
@@ -11,6 +11,6 @@ if [ ! -d "$HOME/dot13" ]; then
     echo "Proceeding.. "
     bash "$HOME/dot13/install/first_time.sh"
 else
-    echo "dot13 is already installed"
-    echo "use $HOME/dot13/install/upgrade.sh"
+    echo 'Already installed'
+    bash "$HOME/dot13/install/upgrade.sh"
 fi
