220413f4039311cc3a1279a228a56ce1af9e2f55
Ernestas Poskus <ernestas.poskus@gmail.com>
2015-09-28 21:31:46 +0300

    Split darwin/linux installations into 2 main files

--- install/install_darwin.sh
+++ install/install_darwin.sh
@@ -1,8 +1,16 @@
-#!/bin/sh
-if ! type -p brew > /dev/null; then
-  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
-fi
+#!/bin/bash
 
-cp -Rv "$HOME/dot13/os/darwin/." "$HOME"
+ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+
+cp -R "$HOME/dot13/os/darwin/." "$HOME"
 
 ln -sf "$HOME/dot13/os/darwin/.darwin_exports" "$HOME"
+
+# Vim
+brew uninstall macvim
+brew remove macvim
+brew cleanup
+brew install macvim --custom-icons --override-system-vim --with-lua --with-luajit
+
+# Zsh
+brew install zsh
