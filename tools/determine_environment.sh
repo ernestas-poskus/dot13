c1a7af63796a4c43d246f66ff6ddf856b54e4dcd
Ernestas Poskus <ernestas.poskus@gmail.com>
2015-09-28 21:16:44 +0300

    Drop tools

--- tools/determine_environment.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ "$(whoami)" == 'ernestasposkus' ]; then
-  echo 'export DOT13_ENV="work"' >> ~/.dot13_environment
-else
-  echo 'export DOT13_ENV="home"' >> ~/.dot13_environment
-fi
