b361372dc3bbc4c550eaa9e4c8b1352950e168e3
Ernestas Poskus <ernestas.poskus@gmail.com>
2015-10-13 17:16:17 +0300

    Remove vim installation from ubuntu bootstrap scrpit

--- install/install_ubuntu.sh
+++ install/install_ubuntu.sh
@@ -15,33 +15,5 @@ sudo apt-get install tmux
 # ctags
 sudo apt-get install exuberant-ctags
 
-# Vim
-sudo apt-get remove vim vim-runtime gvim # Remove old vims
-sudo apt-get remove vim-tiny vim-common vim-gui-common
-
-sudo apt-get install liblua5.1-dev vim-nox
-sudo mkdir /usr/include/lua5.1/include/
-sudo cp /usr/include/lua5.1/* /usr/include/lua5.1/include/
-sudo ln -s /usr/lib/x86_64-linux-gnu/liblua5.1.so /usr/local/lib/liblua.so
-hg clone https://code.google.com/p/vim/
-cd vim
-./configure --with-features=huge \
-    --enable-perlinterp \
-    --enable-rubyinterp \
-    --enable-pythoninterp=yes \
-    --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
-    --enable-multibyte \
-    --enable-fontset \
-    --enable-gui=gtk2 \
-    --disable-netbeans \
-    --enable-luainterp=yes \
-    --with-lua-prefix=/usr/include/lua5.1 \
-    --enable-cscope \
-    --enable-largefile \
-    --prefix=/usr
-make VIMRUNTIMEDIR=/usr/share/vim/vim74
-sudo checkinstall
-make install
-
 # Zsh
 sudo apt-get install -y zsh
