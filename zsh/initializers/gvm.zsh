82cdde238801fdf21cd2c811a7850dfbb897e0b6
Ernestas Poskus <ernestas.poskus@gmail.com>
2015-11-22 22:08:54 +0200

    Add gvm full install if missing

--- zsh/initializers/gvm.zsh
+++ zsh/initializers/gvm.zsh
@@ -1,4 +1,9 @@
 # GVM provides an interface to manage Go versions.
 # https://github.com/moovweb/gvm
+export GVM_DIR="$HOME/.gvm"
+if [ ! -d $GVM_DIR ]; then
+  echo 'Install Golang Version manager'
+  bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+fi
 [[ -s ~/.gvm/scripts/gvm ]] && source ~/.gvm/scripts/gvm
 export GOBIN="$GOPATH"/bin/
