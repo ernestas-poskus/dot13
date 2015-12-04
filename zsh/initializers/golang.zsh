debb9f5dd01d4d6d1ad6bde95b08dd866230254c
Ernestas Poskus <ernestas.poskus@gmail.com>
2015-12-04 17:47:26 +0200

    Merge gvm intitializer with golang

--- zsh/initializers/golang.zsh
+++ zsh/initializers/golang.zsh
@@ -4,3 +4,13 @@ export GO15VENDOREXPERIMENT=1
 if ! type -p gpm > /dev/null; then
 wget https://github.com/pote/gpm/blob/master/bin/gpm && chmod +x gpm && sudo mv gpm /usr/local/bin
 fi
+
+# GVM provides an interface to manage Go versions.
+# https://github.com/moovweb/gvm
+export GVM_DIR="$HOME/.gvm"
+if [ ! -d $GVM_DIR ]; then
+  echo 'Install Golang Version manager'
+  bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+fi
+[[ -s ~/.gvm/scripts/gvm ]] && source ~/.gvm/scripts/gvm
+export GOBIN="$GOPATH"/bin/
