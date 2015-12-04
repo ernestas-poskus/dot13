a25fa8c0e1ff5fff21a180d5c0369e3703afb075
Ernestas Poskus <ernestas.poskus@gmail.com>
2015-12-04 17:46:30 +0200

    Add GPM to golang zsh initializer

--- zsh/initializers/golang.zsh
+++ zsh/initializers/golang.zsh
@@ -1,1 +1,6 @@
 export GO15VENDOREXPERIMENT=1
+
+# GPM: Go Package Manager
+if ! type -p gpm > /dev/null; then
+wget https://github.com/pote/gpm/blob/master/bin/gpm && chmod +x gpm && sudo mv gpm /usr/local/bin
+fi
