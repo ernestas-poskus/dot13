aa365724c0f079e797a7fdd3b87cc238c827b5b1
Ernestas Poskus <ernestas.poskus@gmail.com>
2015-12-23 19:28:38 +0200

    Merge rsvm and rust

--- zsh/initializers/rsvm.zsh
+++
@@ -1,6 +0,0 @@
-export RSVM_DIR="$HOME/.rsvm"
-if [ ! -d $RSVM_DIR ]; then
-  echo 'Installing Rust Version manager'
-  curl -L https://raw.github.com/sdepold/rsvm/master/install.sh | sh
-fi
-[[ -s ~/.rsvm/rsvm.sh ]] && . ~/.rsvm/rsvm.sh # This loads RSVM
--- zsh/initializers/rust.zsh
+++ zsh/initializers/rust.zsh
@@ -1,2 +1,27 @@
-#Rust
-export RUST_SRC_PATH="$HOME/.rsvm/current"
+# RSVM - Rust version manager
+export RSVM_DIR="$HOME/.rsvm"
+if [ ! -d $RSVM_DIR ]; then
+  echo 'Installing Rust Version manager'
+  curl -L https://raw.github.com/sdepold/rsvm/master/install.sh | sh
+fi
+[[ -s ~/.rsvm/rsvm.sh ]] && . ~/.rsvm/rsvm.sh # This loads RSVM
+
+# Rust src
+rust_src="$HOME/rust-src"
+
+# Exporting RUST_SRC_PATH
+export RUST_SRC_PATH="$rust_src/src"
+
+if [ ! -d $RUST_SRC_PATH ]; then
+  git clone https://github.com/rust-lang/rust.git $rust_src
+fi
+
+# Install Rust racer
+if which racer > /dev/null; then
+  cargo install --git 'https://github.com/phildawes/racer.git'
+fi
+
+# Add Carbo bin executables to $PATH
+if [ -d "$HOME/.cargo" ]; then
+  export PATH="$HOME/.cargo/bin:$PATH"
+fi
