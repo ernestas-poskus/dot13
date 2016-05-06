f133ac638e2964a6228261f7264a79a856564c6a
Ernestas Poskus <ernestas.poskus@gmail.com>
2016-05-06 19:53:15 +0300

    xmodmap BackSpace

--- zsh/config/linux.zsh
+++ zsh/config/linux.zsh
@@ -6,5 +6,5 @@ if [ $(uname -s) = 'Linux' ]; then
 
   # Remap keys
   xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape' # Caps Lock -> ESC
-  xmodmap -e 'clear Lock' -e 'keycode 0x6c = BackSpace' # R Alt -> Backspace
+  xmodmap -e 'clear Lock' -e 'keycode 0x108 = BackSpace' # R Alt -> Backspace
 fi
