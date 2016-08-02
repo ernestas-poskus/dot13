cf2cacc9dfd1cfe96a44ffe5601a7f17305ade36
Ernestas Poskus <ernestas.poskus@gmail.com>
2016-08-02 21:35:00 +0300

    Remove golang GPM auto install

--- zsh/initializers/golang.zsh
+++ zsh/initializers/golang.zsh
@@ -1,10 +1,5 @@
 export GO15VENDOREXPERIMENT=1
 
-# GPM: Go Package Manager
-if ! type -p gpm > /dev/null; then
-wget https://github.com/pote/gpm/blob/master/bin/gpm && chmod +x gpm && sudo mv gpm /usr/local/bin
-fi
-
 # GVM provides an interface to manage Go versions.
 # https://github.com/moovweb/gvm
 export GVM_DIR="$HOME/.gvm"
