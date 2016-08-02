0ca25bd63925a05267492287e4c10e8e2f0e6a53
Ernestas Poskus <ernestas.poskus@gmail.com>
2016-08-02 21:35:19 +0300

    Linux check if xmodmap bin exist

--- zsh/config/linux.zsh
+++ zsh/config/linux.zsh
@@ -5,6 +5,8 @@ if [ $(uname -s) = 'Linux' ]; then
   export LC_CTYPE=en_US.UTF-8
 
   # Remap keys
-  xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape' # Caps Lock -> ESC
-  xmodmap -e 'clear Lock' -e 'keycode 0x108 = BackSpace' # R Alt -> Backspace
+  if type -p xmodmap > /dev/null; then
+    xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape' # Caps Lock -> ESC
+    xmodmap -e 'clear Lock' -e 'keycode 0x108 = BackSpace' # R Alt -> Backspace
+  fi
 fi
