e63468cf850d7d4e7d5aa55dfb93a9c62319bf30
Ernestas Poskus <ernestas.poskus@gmail.com>
2016-08-16 20:06:43 +0300

    Add xmodmap lock functionality

--- zsh/config/linux.zsh
+++ zsh/config/linux.zsh
@@ -4,9 +4,14 @@ if [ $(uname -s) = 'Linux' ]; then
   export LC_COLLATE=C
   export LC_CTYPE=en_US.UTF-8
 
+  xmodmap_lock="/tmp/xmodmap.lock"
+
   # Remap keys
   if type -p xmodmap > /dev/null; then
-    xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape' # Caps Lock -> ESC
-    xmodmap -e 'clear Lock' -e 'keycode 0x108 = BackSpace' # R Alt -> Backspace
+    if [ ! -f "$xmodmap_lock" ]; then
+      xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape' # Caps Lock -> ESC
+      xmodmap -e 'clear Lock' -e 'keycode 0x108 = BackSpace' # R Alt -> Backspace
+      touch "$xmodmap_lock"
+    fi
   fi
 fi
