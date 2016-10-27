a5bda0dbe2a70dd69bd654d3c6b656ae5050fd4a
Ernestas Poskus <ernestas.poskus@gmail.com>
2016-10-27 08:50:27 +0300

    Use rsvm current for /home/ow/.rsvm/current/src/rustc-source/src

--- zsh/initializers/rust.zsh
+++ zsh/initializers/rust.zsh
@@ -6,16 +6,8 @@ if [ ! -d $RSVM_DIR ]; then
 fi
 [[ -s ~/.rsvm/rsvm.sh ]] && . ~/.rsvm/rsvm.sh # This loads RSVM
 
-# Rust src
-rust_src="$HOME/.dev/rust-src"
-
 # Exporting RUST_SRC_PATH
-export RUST_SRC_PATH="$rust_src/src"
-
-if [ ! -d $RUST_SRC_PATH ]; then
-  echo "Downloading Rust source $RUST_SRC_PATH"
-  git clone https://github.com/rust-lang/rust.git $rust_src
-fi
+export RUST_SRC_PATH="$HOME/.rsvm/current/src/rustc-source/src"
 
 # Add Carbo bin executables to $PATH
 if [ -d "$HOME/.cargo" ]; then
@@ -25,12 +17,12 @@ fi
 # Install Rust racer
 if ! which racer > /dev/null; then
   echo 'Installing https://github.com/phildawes/racer.git'
-  cargo install --git 'https://github.com/phildawes/racer.git'
+  cargo install -f --git 'https://github.com/phildawes/racer.git'
 fi
 
 if ! which rustfmt > /dev/null; then
   echo 'Installing rustfmt'
-  cargo install rustfmt
+  cargo install -f rustfmt
 fi
 
 if ! which rg > /dev/null; then
