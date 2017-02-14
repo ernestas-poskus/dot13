f79ac82c09a20cb014a25baf4a034e8bcf46790e
Ernestas Poskus <ernestas.poskus@gmail.com>
2017-02-14 12:13:12 +0200

    Add hostname to PS1

--- sh/config/bash.sh
+++ sh/config/bash.sh
@@ -1,4 +1,4 @@
 # PS1 config
-DOT13_PREFIX="\e[91mλ"
+DOT13_PREFIX="\e[91mλ \e[92m$(hostname)"
 DOT13_POSTFIX='$ '
 PS1="$DOT13_PREFIX \e[33m\$(git_current_branch) \[\033[01;34m\]\w\[\033[00m\]\n\$DOT13_POSTFIX"
