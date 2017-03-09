19356faf6c7f925177a946bd89a08f61a6e44d03
Ernestas Poskus <ernestas.poskus@gmail.com>
2017-03-09 07:49:30 +0200

    Add spacemacs javascript mode dev denpendencies

--- sh/initializers/spacemacs.sh
+++ sh/initializers/spacemacs.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
 # SCSS linting
-if dot13_installed_or_mark 'scss_lint'; then
+if dot13_installed_or_mark 'emacs_scss_lint'; then
   gem install specific_install
   gem specific_install https://github.com/brigade/scss-lint.git
   gem specific_install https://github.com/Sweetchuck/scss_lint_reporter_checkstyle.git
+fi
+
+# JavaScript validation
+if dot13_installed_or_mark 'emacs_javascript'; then
+  npm install -g tern
+  npm install -g js-beautify
+  npm install -g eslint
+  npm install -g jshint
 fi
