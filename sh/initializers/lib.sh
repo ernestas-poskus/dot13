1c8d7a5819b1e1862e567eb97d331f2cf44487f7
Ernestas Poskus <ernestas.poskus@gmail.com>
2017-09-14 15:12:13 +0300

    Remove fzy initializer

--- sh/initializers/lib.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-if ! which fzy  > /dev/null; then
-  echo 'Installing fzy fuzzy finder'
-  install_path='/tmp/fzy'
-  git clone https://github.com/jhawthorn/fzy.git $install_path
-  cd $install_path && sudo make install
-fi
