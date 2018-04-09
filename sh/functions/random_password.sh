c0ef4838c94060a6cbba5d8742e0476a59598b15
Ernestas Poskus <ernestas.poskus@gmail.com>
2018-04-09 11:54:14 +0300

    Add passwordsha256 function

--- sh/functions/random_password.sh
+++ sh/functions/random_password.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
 function randompassword() {
   date +%s | sha256sum | base64 | head -c 32 | xsel --clipboard
+}
+
+function passwordsha256() {
+  PASSWORD=$(base64 < /dev/urandom | head -c8); echo "$PASSWORD"; echo -n "$PASSWORD" | sha256sum | tr -d '-'
 }
