6caa37493d1d3de5747c70477ff321bd50cd670b
Ernestas Poskus <ernestas.poskus@gmail.com>
2018-05-20 11:46:01 +0300

    Add curl function curl_stats

--- sh/functions/curl.sh
+++ sh/functions/curl.sh
@@ -14,3 +14,8 @@ function headers() {
 function head_headers() {
   curl -X HEAD -L -v -D - -o /dev/null $@
 }
+
+function curl_status() {
+  format="STATUS %{http_code} ELAPSED %{time_total} URL ${url_effective} ||| redirect: %{time_redirect} namelookup: %{time_namelookup} connect: %{time_connect} appconnect: %{time_appconnect} pretransfer: %{time_pretransfer} starttransfer: %{time_starttransfer}\n"
+  curl -s -L -w "$format" -o /dev/null $1
+}
