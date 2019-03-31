2a0d4b7f8dd709ba2a6b8a01578b838b2faca6a6
Ernestas Poskus <ernestas.poskus@gmail.com>
2019-03-31 11:10:57 +0300

    Add development tools group install to _fedora initializer

--- sh/initializers/_fedora.sh
+++ sh/initializers/_fedora.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if dot13_installed_or_mark 'fedora-dev-tools'; then
+  sudo dnf groupinstall "Development Tools"
+fi
+
 if dot13_installed_or_mark 'fedora-rpmfusion-free-release'; then
   sudo dnf install "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
 fi
