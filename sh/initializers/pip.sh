6f004d2b8d54dad905b2e237c24f5a9a396680a1
Ernestas Poskus <ernestas.poskus@gmail.com>
2019-05-23 20:57:49 +0300

    Add pip pgcli to pip shell initializer

--- sh/initializers/pip.sh
+++ sh/initializers/pip.sh
@@ -16,4 +16,9 @@ if which pip &> /dev/null; then
   if dot13_installed_or_mark 'pip-yamllint'; then
     pip install yamllint --user
   fi
+
+  if dot13_installed_or_mark 'pip-pgcli'; then
+    sudo dnf install redhat-rpm-config python-devel postgresql-devel
+    pip install pgcli --user
+  fi
 fi
